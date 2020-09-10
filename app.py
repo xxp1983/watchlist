6f4de57b0bb2c0d1341c9a569fb87d16612673a3
Sharon Xu <13548422@163.com>
2020-09-10 14:43:43 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,25 +1,69 @@
 # coding=utf-8
 
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+import sys
+import click
 
-name = 'Grey Li'
-movies = [
-{'title': 'My Neighbor Totoro', 'year': '1988'},
-{'title': 'Dead Poets Society', 'year': '1989'},
-{'title': 'A Perfect World', 'year': '1993'},
-{'title': 'Leon', 'year': '1994'},
-{'title': 'Mahjong', 'year': '1996'},
-{'title': 'Swallowtail Butterfly', 'year': '1996'},
-{'title': 'King of Comedy', 'year': '1999'},
-{'title': 'Devils on the Doorstep', 'year': '1999'},
-{'title': 'WALL-E', 'year': '2008'},
-{'title': 'The Pork of Music', 'year': '2012'},
-]
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)
+
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+        click.echo('Drop database.')
+    else:
+        db.create_all()
+        click.echo('Initialized database.')
+
+# 创建数据库模型
+class User(db.Model): # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model): # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+
+    name = 'Sharon'
+    movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
 
 @app.route('/')
-#def hello():
- #   return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
